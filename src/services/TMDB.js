5c1063c2633ce4474401e2e79c2acf33c18b6f8b
Hakan Özcan <hakanozcan20@hotmail.com.tr>
2023-01-20 17:07:20 +0300

    info pages edited

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -24,14 +24,31 @@ export const tmdbApi = createApi({
 
         //* Get Movies by Genre - popular. top_rated, upcoming
         if (genreIdOrCategoryName && typeof genreIdOrCategoryName === 'number') {
-          return `discover/movie?&language=tr&with_genres=${genreIdOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`;
+          return `discover/movie?language=tr&with_genres=${genreIdOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`;
         }
 
         //* Get Popular Movies
-        return `movie/popular?&language=tr&page=${page}&api_key=${tmdbApiKey}`;
+        return `movie/popular?language=tr&page=${page}&api_key=${tmdbApiKey}`;
       },
+    }),
+    // Get Movie
+    getMovie: builder.query({
+      query: (id) => `/movie/${id}?append_to_response=videos,credits&api_key=${tmdbApiKey}&language=tr`,
+    }),
+    // Recommendations
+    getRecommendations: builder.query({
+      query: ({ movie_id, list }) => `/movie/${movie_id}/${list}?api_key=${tmdbApiKey}&language=tr`,
+    }),
+
+    getActorsDetails: builder.query({
+
+      query: (id) => `/person/${id}?api_key=${tmdbApiKey}`,
+    }),
+
+    getMoviesByActorId: builder.query({
+      query: ({ id, page }) => `/discover/movie?with_cast=${id}&page=${page}&api_key=${tmdbApiKey}`,
     }),
   }),
 });
 
-export const { useGetMoviesQuery, useGetGenresQuery } = tmdbApi;
+export const { useGetMoviesQuery, useGetGenresQuery, useGetMovieQuery, useGetRecommendationsQuery, useGetActorsDetailsQuery, useGetMoviesByActorIdQuery } = tmdbApi;
